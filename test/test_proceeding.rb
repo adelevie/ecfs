a119ff6f1d818e2c38f46d0bea98bf08f3ce255e
Alan deLevie <adelevie@gmail.com>
2013-05-19 11:18:19 -0400

    removed comments from proceeding test

--- test/test_proceeding.rb
+++ test/test_proceeding.rb
@@ -6,8 +6,6 @@ class TestProceeding < Test::Unit::TestCase
 
   def test_get_proceeding_info
     VCR.use_cassette('test_proceedings_query_test_get_proceeding_info') do
-      #proceedings_query = ECFS::ProceedingsQuery.new
-      #proceedings_query.eq("docket_number", "12-375")
       results = ECFS::Proceeding.find("12-375")
       %w[
         bureau_name subject date_created status
