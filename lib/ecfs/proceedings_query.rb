5fffc0d0c6fdee44fc606e89939a63c626c0371d
Alan deLevie <adelevie@gmail.com>
2013-05-20 18:05:39 -0400

    refactored proceedings query parser code

--- lib/ecfs/proceedings_query.rb
+++ lib/ecfs/proceedings_query.rb
@@ -72,33 +72,48 @@ module ECFS
 
     def scrape_results_page
       agent = Mechanize.new
-      page = agent.get(self.url)
-      total_pages = page.link_with(:text => "Last").attributes.first[1].split("pageNumber=")[1].gsub(",","").to_i
-      banner = page.search("//*[@id='yui-main']/div/div[2]/table/tbody/tr[2]/td/span[1]").text.lstrip.rstrip.split("Modify Search")[0].rstrip.split  
-      first  = banner[1].gsub(",","").to_i
-      last   = banner[3].gsub(",","").to_i
-      total  = banner[5].gsub(",","").to_i
-      table_rows = page.search("//*[@id='yui-main']/div/div[2]/table/tbody/tr[2]/td/table/tbody").children
-      results   = table_rows.map do |tr|
-        bureau  = tr.children[2].children.children.first.text.lstrip.rstrip
-        subject = tr.children[4].children.text.lstrip.rstrip
-        docket_number = tr.children[0].children[1].attributes["href"].value.split("name=")[1].rstrip
+      page  = agent.get(self.url)
 
-        {
-          "docket_number" => docket_number,
-          "bureau" => bureau,
-          "subject" => subject
-        }
-      end
+      total_pages = page.link_with(:text => "Last").attributes.first[1].split("pageNumber=")[1].gsub(",","").to_i
+      banner      = page.search("//*[@id='yui-main']/div/div[2]/table/tbody/tr[2]/td/span[1]").text.lstrip.rstrip.split("Modify Search")[0].rstrip.split  
+      first       = banner[1].gsub(",","").to_i
+      last        = banner[3].gsub(",","").to_i
+      total       = banner[5].gsub(",","").to_i
+      table_rows  = page.search("//*[@id='yui-main']/div/div[2]/table/tbody/tr[2]/td/table/tbody").children
+      results     = table_rows.map { |row| row_to_hash(row) }
 
       {
-        "current_page" => self.constraints["page_number"].gsub(",","").to_i,
-        "total_pages" => total_pages,
-        "first_result" => first,
-        "last_result" => last,
+        "current_page"  => self.constraints["page_number"].gsub(",","").to_i,
+        "total_pages"   => total_pages,
+        "first_result"  => first,
+        "last_result"   => last,
         "total_results" => total,
-        "results" => results
+        "results"       => results
       }
+    end
+
+    def row_to_hash(row)
+      bureau        = bureau_from_row(row) #tr.children[2].children.children.first.text.lstrip.rstrip
+      subject       = subject_from_row(row) #tr.children[4].children.text.lstrip.rstrip
+      docket_number = docket_number_from_row(row) #tr.children[0].children[1].attributes["href"].value.split("name=")[1].rstrip
+
+      {
+        "docket_number" => docket_number,
+        "bureau"        => bureau,
+        "subject"       => subject
+      }
+    end
+
+    def bureau_from_row(row)
+      row.children[2].children.children.first.text.lstrip.rstrip
+    end
+
+    def docket_number_from_row(row)
+      row.children[0].children[1].attributes["href"].value.split("name=")[1].rstrip
+    end
+
+    def subject_from_row(row)
+      row.children[4].children.text.lstrip.rstrip
     end
 
   end
